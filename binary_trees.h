ad6bab4dd9bbac2ec2e2fa8c93539910162f962d
Juan Sebastian Llano <juanllano93@gmail.com>
2020-07-22 22:49:58 +0000

    Add function to count the nodes, function to measure de balance factor Create functions recursives to count all nodes non-leaf and create a recursive function to calculate de balance factor

--- binary_trees.h
+++ binary_trees.h
@@ -32,7 +32,7 @@ typedef struct binary_tree_s heap_t;
 
 
 /* Prototypes. */
-void binary_tree_print(const binary_tree_t *); /* To print. */
+void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -46,4 +46,6 @@ size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
+size_t binary_tree_nodes(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
 #endif
