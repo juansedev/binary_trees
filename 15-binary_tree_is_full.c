6e5e00202655e50420333e837635ca35243224e4
Juan Sebastian Llano <juanllano93@gmail.com>
2020-07-23 18:25:52 +0000

    Fiex base case for full tree Add validation for tree equal NULL and return 0 in this case

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -10,7 +10,10 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	int r = 0, l = 0;
 
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
 	if (tree->left && tree->right)
