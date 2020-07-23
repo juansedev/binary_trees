c5789356f81ef922c2e214deacf203b852a88e27
Juan Sebastian Llano <juanllano93@gmail.com>
2020-07-23 20:24:06 +0000

    add validation for a full tree in a function to validate perfect tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -25,6 +25,32 @@ size_t binary_tree_height(const binary_tree_t *tree)
 }
 
 /**
+ * binary_tree_is_full - checks if a binary tree is full.
+ * @tree: pointer to root of the tree.
+ *
+ * Return: 1 if is full, 0 otherwise.
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int r = 0, l = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	if (tree->left && tree->right)
+	{
+		l = binary_tree_is_full(tree->left);
+		r = binary_tree_is_full(tree->right);
+	}
+	if (l == r && l == 1)
+		return (1);
+	return (0);
+}
+
+/**
  * binary_tree_is_perfect - function that checks if a binary tree is perfect.
  * @tree: pointer to the root node of the tree to measure the height.
  *
@@ -37,15 +63,15 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->right == NULL && tree->left == NULL)
-		return (1);
-
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 	printf("left %d\n right %d\n", left_height, right_height);
 
 	if (left_height == right_height && tree->left && tree->right)
-		return (1);
+	{
+		if (binary_tree_is_full(tree))
+			return (1);
+	}
 
 	return (0);
 }
