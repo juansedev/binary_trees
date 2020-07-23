8ff72f4a6ff612a5d5438ad590112afb21e9d2d8
Juan Sebastian Llano <juanllano93@gmail.com>
2020-07-23 19:41:27 +0000

    Create function to validate a perfect tree Create a recursive function to measure height and calculate if each node it have 2 childrens

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,41 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measures the height of a binary tree.
+ * @tree: pointer to the root node of the tree to measure the height.
+ *
+ * Return: a size_t value of the hegiht, if tree is NULL return 0.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height = 0, right_height = 0, left_height = 0;
+
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+		height = left_height;
+	else
+		height = right_height;
+
+	return (height + 1);
+}
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int left_height = 0, right_height = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height == right_height && tree->left && tree->right)
+		return (1);
+
+	return (0);
+}
